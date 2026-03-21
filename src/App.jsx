5a136c95485fdd42123bc912b7954c7d2c78dec5
shamsul <webdevlopershamsul@gmail.com>
2026-03-21 18:59:18 +0600

    navbar design complete not responsive:

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import DaisiNav from './Components/DaisiNav.jsx';
+import Navbar from './Navbar/Navbar.jsx';
 
 function App() {
   return (
     <>
 
       <header>
+        <Navbar></Navbar>
         <DaisiNav></DaisiNav>
       </header>
       <main>
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,10 +1,50 @@
+import { list } from 'postcss';
 import React from 'react';
+import Link from './Link';
 
 const Navbar = () => {
+    const navLinks = [
+        {
+            id: 1,
+            name: 'Home',
+            path: '/'
+        },
+        {
+            id: 2,
+            name: 'About Us',
+            path: '/about'
+        },
+        {
+            id: 3,
+            name: 'Services',
+            path: '/services'
+        },
+        {
+            id: 5,
+            name: 'Contact',
+            path: '/contact'
+        },
+        {
+            id: 6,
+            name: 'Blog',
+            path: '/blog'
+        }
+    ];
+
+
     return (
-        <div>
-            
-        </div>
+        <nav>
+            <ul className='flex'>
+                {
+                    navLinks.map(route => <Link key={route.id} route={route}></Link>)
+                }
+            </ul>
+            {/* <ul className='flex'>
+                <li className='mr-10'><a href="/home">Home</a></li>
+                <li className='mr-10'><a href="/about">About</a></li>
+                <li className='mr-10'><a href="/blog">Blog</a></li>
+            </ul > */}
+        </nav >
     );
 };
 
