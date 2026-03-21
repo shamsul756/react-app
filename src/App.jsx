c475d179b26722801df63a63b4259c905f979963
shamsul <webdevlopershamsul@gmail.com>
2026-03-21 17:54:26 +0600

    nav bar create

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import DaisiNav from './Components/DaisiNav.jsx';
+
+function App() {
+  return (
+    <>
+
+      <header>
+        <DaisiNav></DaisiNav>
+      </header>
+      <main>
+
+      </main>
+
+    </>
+  )
+}
+
+export default App
