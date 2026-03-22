39b2dd0ea69ca54398b8de10d56f264f0cab1b04
shamsul <webdevlopershamsul@gmail.com>
2026-03-22 23:58:36 +0600

    completely design card

--- src/Navbar/components/PricingComponents/PricingCard.jsx
+++ src/Navbar/components/PricingComponents/PricingCard.jsx
@@ -4,16 +4,25 @@ import PricingFeature from './PricingFeature';
 const PricingCard = ({ pricing }) => {
    const {name, price, description, features} =pricing;
     return (
-        <div className='border bg-cyan-950'>
+        
+
+       
+        <div className='flex flex-col border bg-cyan-950 m-4'>
             {/* card-header */}
             <h4 className='text-4xl p-4'>{price}</h4>
             <h1 className='text-6xl p-4'>{name}</h1>
+            {/* card-body */}
+            <div className='flex-1'>
             <p className='text-md bg-cyan-900 p-4 rounded-2xl mt-4'>{pricing.subscription} <br />
                 {description}</p>
             {
                 features.map((feature, index)=> <PricingFeature key={index} feature={feature}></PricingFeature>)
             }
+            </div>
+             <button className="btn w-full text-2xl mt-4 hover:bg-cyan-950 border-cyan-800 p-6">Subscribe</button>
         </div>
+       
+     
     );
 };
 
--- src/Navbar/components/PricingOption/PricingOption.jsx
+++ src/Navbar/components/PricingOption/PricingOption.jsx
@@ -1,24 +1,32 @@
 import React, { use } from 'react';
 import PricingCard from '../PricingComponents/PricingCard';
+import DaisiPrisingCard from '../../../Components/DaisiPrisingCard/DaisiPrisingCard';
 
-const PricingOption = ({pricingPromise}) => {
+const PricingOption = ({ pricingPromise }) => {
     const data = use(pricingPromise)
     const pricingArray = data.pricing;
     return (
         <div>
             <h2 className='text-5xl'>Get our membership</h2>
-            
-            <div className='grid md:grid-cols-3 gap-8'>
-                {
+
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
+                {/* {
                     pricingArray.map(pricing => <PricingCard key={pricing.id}
                     pricing={pricing}></PricingCard>)
+                } */}
+                {
+                    pricingArray.map(pricing => <DaisiPrisingCard key={pricing.id}
+                        pricing={pricing}
+                    >
+
+                    </DaisiPrisingCard>)
                 }
             </div>
             <div>
                 {/* pricing card */}
 
-            
-        </div>
+
+            </div>
         </div>
 
     );
